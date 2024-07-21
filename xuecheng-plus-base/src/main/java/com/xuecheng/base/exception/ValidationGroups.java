285ec0258451108e83c0462df24477b66277061c
qianlk <lfqtm@outlook.com>
2024-07-21 10:45:14 +0800

    feat: 内容管理模块

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/ValidationGroups.java
@@ -0,0 +1,16 @@
+package com.xuecheng.base.exception;
+
+/**
+ * @author Qianlk
+ */
+public class ValidationGroups {
+
+    public interface Inster {
+    }
+
+    public interface Update {
+    }
+
+    public interface Delete {
+    }
+}
--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CourseBaseInfoService.java
@@ -2,6 +2,9 @@ package com.xuecheng.content.service;
 
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
+import com.xuecheng.content.model.dto.AddCourseDto;
+import com.xuecheng.content.model.dto.CourseBaseInfoDto;
+import com.xuecheng.content.model.dto.EditCourseDto;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
 
@@ -20,4 +23,32 @@ public interface CourseBaseInfoService  {
      */
     PageResult<CourseBase> queryCourseBaseList(PageParams pageParams, QueryCourseParamsDto queryCourseParamsDto);
 
+    /**
+     * @description 添加课程基本信息
+     * @param companyId  教学机构id
+     * @param addCourseDto  课程基本信息
+     * @return com.xuecheng.content.model.dto.CourseBaseInfoDto
+     * @author Mr.M
+     * @date 2022/9/7 17:51
+     */
+    CourseBaseInfoDto createCourseBase(Long companyId, AddCourseDto addCourseDto);
+
+    /**
+     * @description 根据id查询课程基本信息
+     * @param courseId  课程id
+     * @return com.xuecheng.content.model.dto.CourseBaseInfoDto
+     * @author Mr.M
+     * @date 2022/10/9 8:13
+     */
+    CourseBaseInfoDto getCourseBaseInfo(long courseId);
+
+    /**
+     * @description 修改课程信息
+     * @param companyId  机构id
+     * @param dto  课程信息
+     * @return com.xuecheng.content.model.dto.CourseBaseInfoDto
+     * @author Mr.M
+     * @date 2022/9/8 21:04
+     */
+    public CourseBaseInfoDto updateCourseBase(Long companyId, EditCourseDto dto);
 }
